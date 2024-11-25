0521bb11c028a714e373388be79f758f6a0524ac
anadykhan <aiyoun.khan@gmail.com>
2024-11-26 03:07:05 +0600

    Created search, footer

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,10 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layout/Layout";
+import HomePage from "./pages/HomePage";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout>Home Page</Layout>}></Route>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />
+          </Layout>
+        }
+      ></Route>
       <Route
         path="/user-profile"
         element={<span>User Profile Page</span>}
