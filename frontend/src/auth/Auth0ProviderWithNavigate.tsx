51421bad5293fe91976de10ff751a2a64de5ccbb
anadykhan <aiyoun.khan@gmail.com>
2024-11-27 01:09:32 +0600

    Done sending create user to backend from frontend

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,3 +1,4 @@
+import { useCreateMyUser, userCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 
 type Props = {
@@ -5,6 +6,7 @@ type Props = {
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser, isError, isLoading, isSuccess } = useCreateMyUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -16,7 +18,11 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const onRedirectCallback = (appState?: AppState, user?: User) => {
     console.log("APPSTATE", appState);
     console.log("USER", user);
-  }
+    console.log(user?.sub);
+    if (user?.sub && user?.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
+  };
 
   return (
     <Auth0Provider
@@ -27,7 +33,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
       }}
       onRedirectCallback={onRedirectCallback}
     >
-        {children}
+      {children}
     </Auth0Provider>
   );
 };
